f280dc03e005a2837d96f8b4a3f039a58350ab88
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2018-07-13 17:28:46 -0600

    Se crearon las clases

---
+++ src/Grado.java
@@ -0,0 +1,15 @@
+public class Grado {
+    private int numeroEstudiantes=0;
+    private String[]Estudiantes=new String[numeroEstudiantes];
+    //Getters y setters
+
+
+    public int getNumeroEstudiantes() {
+        return numeroEstudiantes;
+    }
+
+    public void setNumeroEstudiantes(int numeroEstudiantes) {
+        this.numeroEstudiantes = numeroEstudiantes;
+    }
+
+}
